8e0bba04408a68b3a9af92ee121c020da6c43929
Alysha <sh.alyyew2019@gmail.com>
2025-05-19 12:45:16 +0500

    fixed build

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import enTranslation from './locales/en/translation.json';
 import tmTranslation from './locales/tm/translation.json';
