03914896bec001fe37944654d08b23905025ead7
Alysha <sh.alyyew2019@gmail.com>
2025-11-07 00:00:01 +0500

    Fixed changes

--- src/components/Home/Hero.tsx
+++ src/components/Home/Hero.tsx
@@ -1,53 +1,44 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import {
-  OrbitControls,
-  Center,
-  AccumulativeShadows,
-  RandomizedLight,
-} from "@react-three/drei";
-import { AndroidModelViewer } from "./AndroidLoader";
+
 import { Col, Row } from "antd";
 import HeroText from "./HeroText";
-import Loader from "./Loader";
 import useResponsive from "../../hooks/useResponsive";
 
 const Hero = () => {
   const { isMobile } = useResponsive();
 
-  const Hero3D = () => {
-      return(
-          <Canvas
-              shadows
-              camera={{ position: [0, 1.5, -10], fov: 35, zoom: 1.1 }}
-          >
-              <ambientLight intensity={Math.PI} />
-              <spotLight
-                  position={[5, 2, 5]}
-                  angle={0.2}
-                  penumbra={1}
-                  decay={0}
-                  intensity={1}
-                  shadow-mapSize={256}
-                  castShadow
-              />
-              <Suspense fallback={<Loader />}>
-                  <Center top>
-                      <AndroidModelViewer scale={0.6} />
-                  </Center>
-                  <AccumulativeShadows
-                      frames={100}
-                      temporal
-                      alphaTest={0.75}
-                      color="pink"
-                  >
-                      <RandomizedLight radius={8} position={[5, 3, -5]} />
-                  </AccumulativeShadows>
-              </Suspense>
-              <OrbitControls enableZoom={false} autoRotate={true} />
-          </Canvas>
-      )
-  }
+  // const Hero3D = () => {
+  //     return(
+  //         <Canvas
+  //             shadows
+  //             camera={{ position: [0, 1.5, -10], fov: 35, zoom: 1.1 }}
+  //         >
+  //             <ambientLight intensity={Math.PI} />
+  //             <spotLight
+  //                 position={[5, 2, 5]}
+  //                 angle={0.2}
+  //                 penumbra={1}
+  //                 decay={0}
+  //                 intensity={1}
+  //                 shadow-mapSize={256}
+  //                 castShadow
+  //             />
+  //             <Suspense fallback={<Loader />}>
+  //                 <Center top>
+  //                     <AndroidModelViewer scale={0.6} />
+  //                 </Center>
+  //                 <AccumulativeShadows
+  //                     frames={100}
+  //                     temporal
+  //                     alphaTest={0.75}
+  //                     color="pink"
+  //                 >
+  //                     <RandomizedLight radius={8} position={[5, 3, -5]} />
+  //                 </AccumulativeShadows>
+  //             </Suspense>
+  //             <OrbitControls enableZoom={false} autoRotate={true} />
+  //         </Canvas>
+  //     )
+  // }
 
   return (
     <div style={{ overflowX: 'hidden', width: '100%' }}>
@@ -65,13 +56,13 @@ const Hero = () => {
         </Col>
         {!isMobile ? (
           <Col xs={24} md={10} style={{ height: "60vh" }}>
-            <Hero3D/>
+            {/*<Hero3D/>*/}
           </Col>
         ) : null}
       </Row>
-        {
-            isMobile? <div style={{ height: "40vh", marginTop:"22px", marginBottom:"-150px", width: '100%', overflowX: 'hidden' }}><Hero3D/></div>: null
-        }
+        {/*{*/}
+        {/*    isMobile? <div style={{ height: "40vh", marginTop:"22px", marginBottom:"-150px", width: '100%', overflowX: 'hidden' }}><Hero3D/></div>: null*/}
+        {/*}*/}
     </div>
   );
 };
